fbf72a73247d08c4bd3aa829426941ad264408f0
chenchupinggh <1084833640@qq.com>
2016-10-15 11:33:33 +0800

    新的

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  anewdemo
+//
+//  Created by macbook2017 on 16/10/15.
+//  Copyright © 2016年 com.yidahulian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
